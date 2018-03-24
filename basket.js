675e9e9198f0bd0a5940d87ea8d4ac0c3db3891e
Leitino <alessandro.leita89@gmail.com>
2018-03-24 15:09:03 +0100

    esercizio Basket finesett quasi finito

---
+++ basket.js
@@ -0,0 +1,54 @@
+/*Sport Record Book
+Il software deve generare casualmente le statistiche di gioco di 100 giocatori di basket per una giornata di campionato.
+In particolare vanno generate per ogni giocatore le seguenti informazioni, facendo attenzione che il numero generato abbia senso:
+Codice Giocatore Univoco (formato da 3 lettere maiuscole casuali e 3 numeri)
+Numero di punti fatti
+Numero di rimbalzi
+Falli
+Percentuale di successo per tiri da 2 punti
+Percentuale di successo per  da 3 punti
+Una volta generato il “database”, il programma deve chiedere all’utente di inserire un Codice Giocatore e il programma restituisce le statistiche.*/
+
+var giocatori = []
+
+for (var i = 0; i < 100; i++) {
+  var nuovo_giocatore = {
+    'numGiocatore' : randomCode(),
+    'puntiFatti' : casuale(0, 50),
+    'rimbalzi' : casuale(0, 30),
+    'falli' : casuale(0, 10),
+
+  }
+    console.log(nuovo_giocatore)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function randomCode() {
+  var lettere = 'ABCDEFGHILMNOPQRSTUVZ';
+  var numGiocatore = "";
+  for (var i = 0; i < 3; i++) {
+    numGiocatore += lettere.charAt(casuale(0,lettere.length-1));
+  }
+  for (var i = 0; i < 3; i++) {
+    numGiocatore +=casuale(0,9)
+  }
+  return numGiocatore
+}
+
+
+function casuale(inizio,fine) {
+  return Math.floor(Math.random() * (inizio + fine +1)) + inizio;
+}
